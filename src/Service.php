b4a1b1f7c770384a11f6a30effcc6fd1c22f3291
yunwuxin <448901948@qq.com>
2020-05-18 22:02:10 +0800

    路由支持设置前缀

--- src/Service.php
+++ src/Service.php
@@ -8,9 +8,14 @@ class Service extends \think\Service
 {
     public function boot()
     {
-        if ($this->app->config->get('pay.route')) {
-            $this->registerRoutes(function (Route $route) {
-                $route->any("pay/:channel/notify", '\\yunwuxin\\pay\\NotifyController@index')
+        if ($group = $this->app->config->get('pay.route')) {
+            $this->registerRoutes(function (Route $route) use ($group) {
+                if (is_string($group)) {
+                    $rule = "{$group}/pay/:channel/notify";
+                } else {
+                    $rule = "pay/:channel/notify";
+                }
+                $route->any($rule, '\\yunwuxin\\pay\\NotifyController@index')
                     ->completeMatch()
                     ->name('PAY_NOTIFY');
             });
